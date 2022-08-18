e9a36be287a0613c3a24fe51b893aeaf92e47cbb
Jason Ford <jasford@adobe.com>
2022-08-18 10:40:09 +1000

    Initial commit

---
+++ src/Components/QRReader/QRReader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const QRReader = () => {
+    return <div>QRReader</div>;
+};
+
+export default QRReader;
---
+++ src/overrides/venia-ui/components/Header/header.js
@@ -0,0 +1,171 @@
+import React, { Fragment, Suspense, useEffect, useState } from 'react';
+import { shape, string } from 'prop-types';
+import { Link, Route, Redirect, useHistory } from 'react-router-dom';
+
+import Logo from '@magento/venia-ui/lib/components/Logo';
+import AccountTrigger from '@magento/venia-ui/lib/components/Header/accountTrigger';
+import CartTrigger from '@magento/venia-ui/lib/components/Header/cartTrigger';
+import NavTrigger from '@magento/venia-ui/lib/components/Header/navTrigger';
+import SearchTrigger from '@magento/venia-ui/lib/components/Header/searchTrigger';
+import OnlineIndicator from '@magento/venia-ui/lib/components/Header/onlineIndicator';
+import { useHeader } from '@magento/peregrine/lib/talons/Header/useHeader';
+import resourceUrl from '@magento/peregrine/lib/util/makeUrl';
+
+import { useStyle } from '@magento/venia-ui/lib/classify';
+import defaultClasses from '@magento/venia-ui/lib/components/Header/header.module.css';
+import StoreSwitcher from '@magento/venia-ui/lib/components/Header/storeSwitcher';
+import CurrencySwitcher from '@magento/venia-ui/lib/components/Header/currencySwitcher';
+import MegaMenu from '@magento/venia-ui/lib/components/MegaMenu';
+import PageLoadingIndicator from '@magento/venia-ui/lib/components/PageLoadingIndicator';
+import { useIntl } from 'react-intl';
+import { QrReader } from 'react-qr-reader';
+import QRIcon from './QRIcon.svg';
+
+const SearchBar = React.lazy(() =>
+    import('@magento/venia-ui/lib/components/SearchBar')
+);
+
+const Header = props => {
+    const {
+        handleSearchTriggerClick,
+        hasBeenOffline,
+        isOnline,
+        isSearchOpen,
+        searchRef,
+        searchTriggerRef
+    } = useHeader();
+
+    const classes = useStyle(defaultClasses, props.classes);
+    const rootClass = isSearchOpen ? classes.open : classes.closed;
+
+    const searchBarFallback = (
+        <div className={classes.searchFallback} ref={searchRef}>
+            <div className={classes.input}>
+                <div className={classes.loader}>
+                    <div className={classes.loaderBefore} />
+                    <div className={classes.loaderAfter} />
+                </div>
+            </div>
+        </div>
+    );
+    const searchBar = isSearchOpen ? (
+        <Suspense fallback={searchBarFallback}>
+            <Route>
+                <SearchBar isOpen={isSearchOpen} ref={searchRef} />
+            </Route>
+        </Suspense>
+    ) : null;
+
+    const { formatMessage } = useIntl();
+    const title = formatMessage({ id: 'logo.title', defaultMessage: 'Venia' });
+
+    const [showQRScanner, setShowQRScanner] = useState(false);
+    const [QRDestination, setQRDestination] = useState();
+    const history = useHistory();
+
+    useEffect(() => {
+        setShowQRScanner(false);
+    }, [QRDestination]);
+
+    if (QRDestination) {
+        //return <Redirect to={QRDestination} />;
+        history.push(QRDestination);
+        setQRDestination();
+    }
+
+    return (
+        <Fragment>
+            <div className={classes.switchersContainer}>
+                <div className={classes.switchers} data-cy="Header-switchers">
+                    <StoreSwitcher />
+                    <CurrencySwitcher />
+                </div>
+            </div>
+            <header className={rootClass} data-cy="Header-root">
+                <div className={classes.toolbar}>
+                    <div className={classes.primaryActions}>
+                        <NavTrigger />
+                        <div className="flex items-center">
+                            <span
+                                className="flex items-center"
+                                onPointerDown={e => {
+                                    e.preventDefault();
+                                    setShowQRScanner(!showQRScanner);
+                                    console.log('here');
+                                }}
+                            >
+                                <img
+                                    src={QRIcon}
+                                    alt="QR Scanner"
+                                    width="30px"
+                                />
+                                <span className="hidden sm_inline sm_ml-1">
+                                    QR Scanner
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                    <OnlineIndicator
+                        hasBeenOffline={hasBeenOffline}
+                        isOnline={isOnline}
+                    />
+                    <Link
+                        aria-label={title}
+                        to={resourceUrl('/')}
+                        className={classes.logoContainer}
+                        data-cy="Header-logoContainer"
+                    >
+                        <Logo classes={{ logo: classes.logo }} />
+                    </Link>
+                    <MegaMenu />
+                    <div className={classes.secondaryActions}>
+                        <SearchTrigger
+                            onClick={handleSearchTriggerClick}
+                            ref={searchTriggerRef}
+                        />
+                        <AccountTrigger />
+                        <CartTrigger />
+                    </div>
+                </div>
+                {searchBar}
+                <PageLoadingIndicator absolute />
+            </header>
+
+            <div>
+                {showQRScanner && (
+                    <QrReader
+                        onResult={(result, error) => {
+                            console.log('result', result);
+                            //console.log('error', error);
+
+                            if (result) {
+                                if (result.text) {
+                                    setQRDestination(result.text);
+                                }
+                            }
+
+                            // if (!!error) {
+                            //     console.error(error);
+                            // }
+                        }}
+                    />
+                )}
+            </div>
+        </Fragment>
+    );
+};
+
+Header.propTypes = {
+    classes: shape({
+        closed: string,
+        logo: string,
+        open: string,
+        primaryActions: string,
+        secondaryActions: string,
+        toolbar: string,
+        switchers: string,
+        switchersContainer: string
+    })
+};
+
+export default Header;
